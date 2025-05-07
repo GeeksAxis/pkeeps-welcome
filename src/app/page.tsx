4e53e31b12a9fcc3e8115701146da3e26aa3398e
codeswot <elmubarak333@gmail.com>
2025-05-07 19:01:17 +0100

    add favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-"use client";
 import Footer from "@/components/custom/Footer";
 import { Hero } from "@/components/custom/Hero";
 import MetricsStatistics from "@/components/custom/Metric-statistics";
